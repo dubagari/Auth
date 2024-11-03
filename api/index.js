cf4bc98e731e6c18440be9b5cebef6733b4bc83d
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2024-11-03 05:57:20 +0100

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch(() => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("server is running at the port 3000!");
